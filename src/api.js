a177d44652a045328e28eaed756cb281f4420374
Meredith Baird <mnbaird@Meredith-Bairds-MacBook-Pro.local>
2017-06-18 17:27:44 -0600

    working google api call on it's own server

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import path from 'path';
 var bodyParser = require('body-parser');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var parseString = require('xml2js').parseString;
+import axios from 'axios';
 const app = express();
 import cors from 'cors';
+var request = require('request');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,40 +13,47 @@ app.use(cors());
 var router = express.Router();
 
 
+
 // app.use(express.static('src/api.js'));
 //
 // app.get('/',(req, res) =>{
 //   res.sendFile('index.html');
 // });
-
-var xhr = new XMLHttpRequest();
-
+app.use(function(req,res,next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 router.use(function(res, req, next) {
   console.log("something is happening");
   next();
-})
+});
 
 router.get('/',function(req, res) {
   res.json({ message: "Hello, welcome to our api!"})
 })
 
-router.route('/info')
+router.route('/places')
 
   .get(function(req, res) {
-    xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200){
-        parseString(xhr.responseText, function(err, result) {
-          res.json(result.nlmSearchResult.list[0].document[0]);
-        });
-      }
-    };
-    // parseString(xhr, function(err, result) {
-    //   console.dir(result);
-    // });
-    xhr.open('GET', "https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term=title:asthma&retmax=5", true);
-    xhr.send();
-  })
+    // req.pipe(request
+    // let type="pharmacy";
+    let url=
+    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=46.878178,-114.001003&radius=5000&key=AIzaSyDiIK5Y8YpXKY5_aVv5noyqmPRspT160JE&type=pharmacy";
+    req.pipe(request(url)).pipe(res);
+    // request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=46.878178,-114.001003&radius=5000&type=pharmacy&key=AIzaSyDiIK5Y8YpXKY5_aVv5noyqmPRspT160JE", function (error, response, body) {
+    //     console.log(error); // Print the error if one occurred
+    //     console.log(response.statusCode); // Print the response status code if a response was received
+    //     console.log(JSON.parse(body));
+
+        // res.json(response);
+        // res.send(response);
+  });
+
+// })
+  //   'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=46.878178,-114.001003&radius=5000&type=pharmacy&key=AIzaSyDiIK5Y8YpXKY5_aVv5noyqmPRspT160JE',result => res.json(result));
+  // })
 
 app.use('/api', router);
 app.listen(3003,()=>console.log("Listening on port 3003"));
