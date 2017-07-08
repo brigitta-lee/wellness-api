38ba37efc9e086d7f9588b4aafdc62d3d5aaddac
Jennifer Emerson <jenniferemerson@Jennifers-MacBook-Air.local>
2017-07-08 16:28:13 -0600

    changed port to 5000

--- src/api.js
+++ src/api.js
@@ -47,4 +47,4 @@ router.route('/map')
 
 
 app.use('/api', router);
-app.listen(process.env.PORT || 3003,()=>console.log("Listening on port 3003"));
+app.listen(process.env.PORT || 5000,()=>console.log("Listening on port 3003"));
